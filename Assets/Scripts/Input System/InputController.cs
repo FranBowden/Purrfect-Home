47d665c10a9cfc2eb069a2f48b74638febdba044
FranBowden <francesca.bowden22@bathspa.ac.uk>
2025-03-07 15:00:05 +0000

    Player can now move horizontally

--- Assets/Scripts/Input System/InputController.cs
+++ Assets/Scripts/Input System/InputController.cs
@@ -5,11 +5,13 @@ public class InputController : MonoBehaviour
 {
     private InputSystem_Actions inputSystem;
     private InputSystem_Actions.PlayerActions player;
+    private PlayerMovement movement;
     
     private void Awake()
     {
         inputSystem = new InputSystem_Actions();
         player = inputSystem.Player;
+        movement = GetComponent<PlayerMovement>();
 
 
         //example
@@ -17,6 +19,10 @@ public class InputController : MonoBehaviour
         //player.pancamera.performed += ctx => function();
     }
 
+    private void FixedUpdate()
+    {
+        movement.move(player.Move.ReadValue<Vector2>());
+    }
     private void OnEnable()
     {
         player.Enable();
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    public float speed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void move(Vector2 input)
+    {
+        Vector3 movementDirection = Vector3.zero;
+        movementDirection.x = input.x;
+        movementDirection.y = 0;
+        controller.Move(transform.TransformDirection(movementDirection) * speed * Time.deltaTime);
+    }
+    
+}
